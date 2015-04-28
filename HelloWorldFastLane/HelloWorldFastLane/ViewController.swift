005fce886ba2cc19073fc080096a305599e462ee
Amund Tveit <atveit@gmail.com>
2015-04-28 12:35:02 +0200

    initial commit

---
+++ HelloWorldFastLane/HelloWorldFastLane/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  HelloWorldFastLane
+//
+//  Created by Amund Tveit on 28/04/15.
+//  Copyright (c) 2015 Amund Tveit. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
